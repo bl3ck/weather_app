57530d1cecadb4997fc27e9eaea21d69b16166c8
Eugen-Bleck <eugenbleck@gmail.com>
2020-08-11 12:59:18 +0100

    refactor: moves zip into buttonclick event

--- website/app.js
+++ website/app.js
@@ -1,10 +1,10 @@
 
 /* Global Variables */
 const URL = "http://api.openweathermap.org/data/2.5/weather"
-const API_KEY ="708f11cf0ed3d72a66e148d077ba6fef"
-const getWeatherMapData = async (zip) =>  {
+const API_KEY = "708f11cf0ed3d72a66e148d077ba6fef"
+const getWeatherMapData = async (zip) => {
     try {
-        const result = await fetch(URL+`?zip=${zip}&appid=${API_KEY}`, {
+        const result = await fetch(URL + `?zip=${zip}&appid=${API_KEY}`, {
             mode: 'cors'
         })
         // console.log(" resp ", result)
@@ -13,7 +13,7 @@ const getWeatherMapData = async (zip) =>  {
         console.log(" some errors occured", error)
     }
 }
-const updateUI = async ( data ) =>  {
+const updateUI = async (data) => {
     let uiData = await data;
     console.log(uiData);
     document.getElementById("date").innerHTML = uiData.date
@@ -21,14 +21,14 @@ const updateUI = async ( data ) =>  {
     document.getElementById("temp").innerHTML = uiData.temperature
 }
 
-const syncWeatherMapData = async (path, data) =>  {
+const syncWeatherMapData = async (path, data) => {
     try {
-        console.log(" sendign th following" , data)
+        console.log(" sendign th following", data)
         const result = await fetch(path, {
             mode: 'cors',
             body: JSON.stringify(data),
             method: "POST",
-            headers : {
+            headers: {
                 "Content-type": 'application/json'
             }
         })
@@ -41,22 +41,23 @@ const syncWeatherMapData = async (path, data) =>  {
 
 }
 
-const zipCode = document.getElementById('zip').value
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear()
 
 
 let buttonGenerate = document.getElementById('generate')
 buttonGenerate.addEventListener('click', async () => {
     try {
-        let response  = await getWeatherMapData(zipCode)
+        const zipCode = document.getElementById('zip').value
+        // Create a new date instance dynamically with JS
+        let d = new Date();
+        let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear()
+
+        let response = await getWeatherMapData(zipCode)
         console.log(response)
         // const date = document.getElementById("date")
         const feelings = document.getElementById("feelings").value
         const data = { date: newDate, temperature: response.main.temp, content: feelings, apiData: response }
-        await syncWeatherMapData("http://localhost:3000/weather", data )
-            
+        await syncWeatherMapData("http://localhost:3000/weather", data)
+
     } catch (error) {
         console.log(error)
     }
