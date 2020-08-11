eb80d7b5e73dc08cc4d984390604d9790399801b
Eugen-Bleck <eugenbleck@gmail.com>
2020-08-11 09:18:29 +0100

    feat: implements updateUI, and sync functions

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
 const express = require('express');
@@ -27,6 +27,19 @@ app.listen(PORT, () => {
     console.log(`Server running\nOn port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    console.log('Home page', res.body);
+app.get('/weather', (req, res) => {
+
+    res.send(projectData).status('200'); 
+    
+});
+
+app.post('/weather', (req, res) => {
+    const { temperature, date, content, apiData } = req.body
+    // console.log('Req', req.body)
+    // console.log(req.query.max)
+
+    let newp = [...projectData, {temperature,date, content}];
+    projectData = newp;
+    console.log('Data: ', projectData);
+    res.send({temperature, date, content}).status('201')
 });
--- website/app.js
+++ website/app.js
@@ -1,5 +1,63 @@
-/* Global Variables */
 
+/* Global Variables */
+const URL = "http://api.openweathermap.org/data/2.5/weather"
+const API_KEY ="708f11cf0ed3d72a66e148d077ba6fef"
+const getWeatherMapData = async (zip) =>  {
+    try {
+        const result = await fetch(URL+`?zip=${zip}&appid=${API_KEY}`, {
+            mode: 'cors'
+        })
+        // console.log(" resp ", result)
+        return result.json()
+    } catch (error) {
+        console.log(" some errors occured", error)
+    }
+}
+const updateUI = async ( data ) =>  {
+    let uiData = await data;
+    console.log(uiData);
+    document.getElementById("date").innerHTML = uiData.date
+    document.getElementById("content").innerHTML = uiData.content
+    document.getElementById("temp").innerHTML = uiData.temperature
+}
+
+const syncWeatherMapData = async (path, data) =>  {
+    try {
+        console.log(" sendign th following" , data)
+        const result = await fetch(path, {
+            mode: 'cors',
+            body: JSON.stringify(data),
+            method: "POST",
+            headers : {
+                "Content-type": 'application/json'
+            }
+        })
+        // console.log(" resp ", result)
+        // return result.json()
+        await updateUI(result.json())
+    } catch (error) {
+        console.log(" some errors occured", error)
+    }
+
+}
+
+const zipCode = document.getElementById('zip').value
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear()
+
+
+let buttonGenerate = document.getElementById('generate')
+buttonGenerate.addEventListener('click', async () => {
+    try {
+        let response  = await getWeatherMapData(zipCode)
+        console.log(response)
+        // const date = document.getElementById("date")
+        const feelings = document.getElementById("feelings").value
+        const data = { date: newDate, temperature: response.main.temp, content: feelings, apiData: response }
+        await syncWeatherMapData("http://localhost:3000/weather", data )
+            
+    } catch (error) {
+        console.log(error)
+    }
+})
